8282af53e926ef8c7d9adfadab81934fb2ef3ade
Felix Doura <felixdoura@gmail.com>
2024-06-01 10:18:27 -0300

    last commit for the budget tracker mini project

--- budget-tracker.py
+++ budget-tracker.py
@@ -1,3 +1,5 @@
+import json
+
 
 def add_expense(expenses, description, amount):
     expenses.append({"description": description, "amount": amount})
@@ -20,13 +22,30 @@ def show_budget_details(budget, expenses):
     print(f"Total Spent: {get_total_expenses(expenses)}")
     print(f"Remaining Budget: {get_balance(budget, expenses)}")
 
+def load_budget_data(filepath):
+    try:
+        with open(filepath, 'r') as file:
+            data = json.loads(file)
+            return data["initial_budget"], data["expenses"]
+    except (FileNotFoundError, json.JSONDecodeError):
+        return 0, []
+
+def save_budget_details(filepath, initial_budget, expenses):
+    data = {
+        'initial_budget': initial_budget,
+        'expenses': expenses
+    }
+    with open(filepath, 'w') as file:
+        json.dump(data, file, indent=4)
+
 def main():
     print("Welcome to the Budget App")
     initial_budget = float(input("Please enter Your initial budget: "))
-    # filepath = 'budget_data.json'
-    # initial_budget, expenses = load_budget_data(filepath)
+    filepath = 'budget_data.json'
+    initial_budget, expenses = load_budget_data(filepath)
+    if initial_budget == 0:
+        initial_budget = float(input("Please enter your initial budget: "))
     budget = initial_budget
-    expenses = []
 
     while True:
         print("\nWhat would You like to do?")
@@ -42,6 +61,7 @@ def main():
         elif choice == "2":
             show_budget_details(budget, expenses)
         elif choice == "3":
+            save_budget_details(filepath, initial_budget, expenses)
             print("Exiting Budget App...")
             break
         else:
